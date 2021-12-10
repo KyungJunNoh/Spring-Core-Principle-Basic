22e26353ba2227abe0733d18d736d68524cd9498
노경준 <developerjun0615@gmail.com>
2021-12-10 12:01:43 +0900

    Update MemberServiceImpl 주석 작성

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    // 현재는 역할(MemberRepository)과 구현(MemoryMemberRepository) 에 모두 의존 되어있음. (DIP가 지켜지지 않음)
     private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     @Override
