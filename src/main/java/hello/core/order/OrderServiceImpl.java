6a843c36b0136c55f793e5df81dc3f392ada053b
노경준 <developerjun0615@gmail.com>
2021-12-10 13:41:59 +0900

    [주문과 할인 도메인 개발] ADD OrderServiceImpl Order 서비스 구현 클래스 생성

---
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice); // 설계가 잘된 예시) SRP - 단일 책임의 원칙 가 잘 지켜짐
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
