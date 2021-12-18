36e997b5c18dab931b39e52d42589455dd73c2bf
노경준 <developerjun0615@gmail.com>
2021-12-18 18:32:53 +0900

    [스프링으로 전환하기] Update AppConfig 스프링 도입

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,19 +9,25 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 // DI ( Dependency Injection - 의존성 주입 )
 // 애플리케이션의 실제 동작에 필요한 구현 객체를 생성 한다.
+@Configuration // 애플리케이션의 설정 정보 선언
 public class AppConfig {
 
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository()); // 생성자 주입
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository(); // 추후에 다른 구현체로 바꿀때 이 부분만 바꿔주면 된다.
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(
                 memberRepository(),
@@ -29,7 +35,8 @@ public class AppConfig {
         );
     }
 
-    private DiscountPolicy discountPolicy(){
+    @Bean
+    public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy(); // 추후에 다른 구현체로 바꿀때 이 부분만 바꿔주면 된다.
     }
 
