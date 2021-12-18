f45839ea0212e133cfef77b40eb49b165939f1ba
노경준 <developerjun0615@gmail.com>
2021-12-18 18:33:10 +0900

    [스프링으로 전환하기] Update OrderApp 스프링 도입

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,14 +7,20 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
 
     public static void main(String[] args) {
 
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class); // 스프링의 컨테이너를 관리해주는 ApplicationContext
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); // 빈 으로 등록되어있는 메서드를 가져옴.
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class); // 빈 으로 등록되어있는 메서드를 가져옴.
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
