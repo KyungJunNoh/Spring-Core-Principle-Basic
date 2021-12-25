a490b5c583a1c6ea5ea90d3f69cf16e720912a95
노경준 <developerjun0615@gmail.com>
2021-12-25 19:25:37 +0900

    [탐색 위치와 기본 스캔 대상] Update AutoAppConfig @ComponentScan 사용을 위한 어노테이션 추가

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,15 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "hello.core.member", // 스캔(빈 등록) 하고싶은 패키지를 지정
+        basePackageClasses = AutoAppConfig.class, // 해당 클래스가 있는 패키지를 스캔(빈 등록)
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class) // Configuration 어노테이션이 붙은 코드는 거르겠다는 코드
+)
+public class AutoAppConfig {
+
+}
