e48ae4603aa0e75efe4a1c72f071dd9113f3518e
노경준 <developerjun0615@gmail.com>
2021-12-25 20:13:03 +0900

    [중복 등록과 충돌] Update MemoryMemberRepository 주석 추가

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Component;
 import java.util.HashMap;
 import java.util.Map;
 
-@Component
+@Component // defaultName : memoryMemberRepository
 public class MemoryMemberRepository implements MemberRepository {
 
     private static Map<Long,Member> store = new HashMap<>();
