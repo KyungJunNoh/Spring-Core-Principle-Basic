eae6dcc24e4c9bdf1f4a3ac98251c19410aef7b9
노경준 <developerjun0615@gmail.com>
2021-12-25 19:29:48 +0900

    [탐색 위치와 기본 스캔 대상] Update MemberServiceImpl @ComponentScan 을 사용하기 위한 @Component, @Autowired 추가

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 // MemberServiceImpl 입장에서는 생성자를 통해 어떤 구현 객체가 들어올지(주입될지)는 알 수 없다.
 // MemberServiceImpl 의 생성를 통해서 어떤 구현 객체를 주입할건지 오직 외부('AppConfig')에서 결정된다.
 // MemberServiceImpl 은 이제 의존관계에대한 고민은 외부에서 맡기고 실행에만 집중하면 된다.
@@ -8,6 +12,7 @@ public class MemberServiceImpl implements MemberService{
     // 설계 변경으로 MemberServiceImpl은 MemoryMemberRepository( 구현체 ) 를 의존하지 않는다!
     private final MemberRepository memberRepository; // 추상화에만 의존하게 변경
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
