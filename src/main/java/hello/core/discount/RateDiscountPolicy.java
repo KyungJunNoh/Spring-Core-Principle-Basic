597f72a53811865d2b56363b3c1931c6115fcb90
노경준 <developerjun0615@gmail.com>
2022-01-08 15:14:52 +0900

    [애노테이션 직접 만들기] Update MainDiscountPolicy 직접 만든 어노테이션 적용

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -1,12 +1,13 @@
 package hello.core.discount;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Qualifier("mainDiscountPolicy") // 구분자를 추가
+@MainDiscountPolicy // 구분자를 추가
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
