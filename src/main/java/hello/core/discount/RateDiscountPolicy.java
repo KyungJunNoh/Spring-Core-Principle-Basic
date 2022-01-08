724c3d4293f9fa0f360a4ded9809d798ab3eda84
노경준 <developerjun0615@gmail.com>
2022-01-08 15:03:10 +0900

    [@Autowired 필드 명, @Qualifier, @Primary] Update RateDiscountPolicy 빈 주입 우선순위를 위한 Qualifier 지정

--- src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,9 +2,11 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("mainDiscountPolicy") // 구분자를 추가
 public class RateDiscountPolicy implements DiscountPolicy{
 
     private int discountPercent = 10;
