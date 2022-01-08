c3cb14eb5b24e3dc2fc85a6b583d9193fd1f7a2f
노경준 <developerjun0615@gmail.com>
2022-01-08 15:03:32 +0900

    [@Autowired 필드 명, @Qualifier, @Primary] Update OrderServiceImpl 주석 추가

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,6 +8,7 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,8 +18,8 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository; // 인터페이스에만 의존하도록 변경 ( DIP 지킴 )
     private final DiscountPolicy discountPolicy; // 인터페이스에만 의존하도록 변경 ( DIP 지킴 )
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    @Autowired // @Autowired 의 생성자 빈 조회 방법은 타입을 먼저 조회한 다음에 같은 타입의 빈이 여러개라면 그 다음에 이름으로 조회를 한다. 그래서 파라미터에 주입을 받을 이름을 입력해주면 빈이 여러개여도 주입이 가능하다.
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) { // 같은 Qualifier 을 찾아서 매핑
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
