576d29a260d096a09cb287581a95a249f6f821e7
노경준 <developerjun0615@gmail.com>
2022-01-08 14:24:02 +0900

    [조회 빈이 2개 이상 - 문제] Update OrderServiceImpl 빈이 2개 이상일때 생성자 주입 문제

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -11,11 +11,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final 이 붙은 필드에 생성자를 주입 // lombok 에서 지원
+// @RequiredArgsConstructor // final 이 붙은 필드에 생성자를 주입 // lombok 에서 지원
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository; // 인터페이스에만 의존하도록 변경 ( DIP 지킴 )
     private final DiscountPolicy discountPolicy; // 인터페이스에만 의존하도록 변경 ( DIP 지킴 )
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
