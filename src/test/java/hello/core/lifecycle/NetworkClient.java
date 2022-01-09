261c3fbfe88e92c0e0ad1ab72bf461d9f5ec0b4a
노경준 <developerjun0615@gmail.com>
2022-01-09 19:57:59 +0900

    [빈 등록 초기화, 소멸 메서드] Update NetworkClient @Bean 어노테이션 설정 값 으로 초기화 삭제 사용

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
 
     private String url;
 
@@ -29,14 +29,14 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception { // 의존관계 주입이 끝나면 호출하겠다는 메서드
+
+    public void init() { // 의존관계 주입이 끝나면 호출하겠다는 메서드
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
-    @Override
-    public void destroy() throws Exception { // 빈이 종료될때 호출
+    public void close() { // 빈이 종료될때 호출
         System.out.println("NetworkClient.destroy");
         disconnect();
     }
