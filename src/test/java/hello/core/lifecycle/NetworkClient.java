7074def57ca2c973af8110c886b66a38793b923c
노경준 <developerjun0615@gmail.com>
2022-01-09 19:41:22 +0900

    [인터페이스 InitializingBean, DisposableBean] Update NetworkClient InitializingBean 과 DisposableBean 구현 하여 초기화와 종료를 관리

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메세지");
     }
 
     public void setUrl(String url){
@@ -28,7 +29,15 @@ public class NetworkClient {
         System.out.println("close " + url);
     }
 
-    public static void main(String[] args) {
+    @Override
+    public void afterPropertiesSet() throws Exception { // 의존관계 주입이 끝나면 호출하겠다는 메서드
+        connect();
+        call("초기화 연결 메세지");
+    }
 
+    @Override
+    public void destroy() throws Exception { // 빈이 종료될때 호출
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
