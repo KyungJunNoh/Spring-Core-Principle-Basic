3ca91d646df09240330d4da6458cf4dded1d3d00
노경준 <developerjun0615@gmail.com>
2022-01-10 17:09:04 +0900

    [request 스코프 예제 만들기] ADD MyLogger log 를 찍을 수 있는 MyLogger 생성

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,36 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request") // 이 빈의 생존 범위는 request 이다. ( 고객으로 부터 요청이 들어왔을 때만 생존 인데 요청이 들어오지 않아 DI 를 할때 에러가 터진다. )
+// ( 스프링 애플리케이션을 실행하는 시점에 싱글톤 빈은 생성해서 주입이 가능하지만, request 스코프 빈은 아직 생성되지 않는다. 이 빈은 실제 고객의 요청이 와야 생성할 수 있다! )
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message){
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + message);
+    }
+
+    @PostConstruct
+    public void init(){
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create : " + this);
+    }
+
+    @PreDestroy
+    public void close(){
+        System.out.println("[" + uuid + "] request scope bean close : " + this);
+    }
+}
