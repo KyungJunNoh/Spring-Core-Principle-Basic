90f2cf4e4aecfdb7ebaa94a3d929b0b5cd96f3d8
노경준 <developerjun0615@gmail.com>
2022-01-10 17:22:10 +0900

    [스코프와 Provider] Update LogDemoController Provider를 사용하여 스프링 구동 시에도 DI 가 가능하도록 변경

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,6 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -13,15 +14,17 @@ import javax.servlet.http.HttpServletRequest;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final MyLogger myLogger;
+    private final ObjectProvider<MyLogger> myLoggerObjectProvider; // 스프링 구동 시 의존관계를 주입 받을 수 있다.
 
     @RequestMapping("log-demo")
     @ResponseBody // String 형태를 화면에 출력
-    public String logDemo(HttpServletRequest request) { // 자바에서 제공하는 표준 서블릿 정보에서 request 를 가져옴
+    public String logDemo(HttpServletRequest request) throws InterruptedException { // 자바에서 제공하는 표준 서블릿 정보에서 request 를 가져옴
         String requestUrl = request.getRequestURL().toString();
+        MyLogger myLogger = myLoggerObjectProvider.getObject();
         myLogger.setRequestURL(requestUrl);
-        myLogger.log("controller test");
 
+        myLogger.log("controller test");
+        Thread.sleep(2000);
         logDemoService.logic("testId");
         return "OK";
     }
