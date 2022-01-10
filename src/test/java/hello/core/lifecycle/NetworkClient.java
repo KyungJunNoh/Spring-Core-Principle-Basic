72baaa430fc8e2b59d0869fadf6c51bbddcaf9b5
노경준 <developerjun0615@gmail.com>
2022-01-10 14:40:25 +0900

    [애노테이션 @PostConstruct, @PreDestroy] NetworkClient 어노테이션을 사용하여 빈생명주기를 관리

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -3,6 +3,9 @@ package hello.core.lifecycle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -29,13 +32,14 @@ public class NetworkClient {
         System.out.println("close " + url);
     }
 
-
+    @PostConstruct
     public void init() { // 의존관계 주입이 끝나면 호출하겠다는 메서드
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메세지");
     }
 
+    @PreDestroy
     public void close() { // 빈이 종료될때 호출
         System.out.println("NetworkClient.destroy");
         disconnect();
